56c3edb7b615817318d288855b343ab41da2b275
Jared Wright <jtw57@gmail.com>
2015-03-28 17:05:37 -0400

    working server

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var io = require('socket.io').listen(4000);
+var http = require("http");
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("ssjc-app");
+}).listen(process.env.PORT || 4000);
+
+var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
 	console.log('Signaling connection started');
+
+	socket.on('clientReq', function (data) {
+	    socket.emit('reqWebkit',data);
+	});
 
 	socket.on('webkitStream', function (data) {
 	    socket.emit('clientStream',data);
